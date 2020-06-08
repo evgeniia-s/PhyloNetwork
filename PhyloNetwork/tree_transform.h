5293cc941d764e566270980cf6b280ade1f86b5f
evgeniia-s <66036698+evgeniia-s@users.noreply.github.com>
2020-06-09 00:29:04 +0300

    Adding string to tree transfer

--- PhyloNetwork/tree_transform.h
+++ PhyloNetwork/tree_transform.h
@@ -2,6 +2,8 @@
 #include <string>
 #include "node.h"
 
-node* string_to_tree(string src); 
+node* string_to_tree(string src);
 
-node* string_to_tree_recur(string src);
+node* string_to_tree_recur(string src, int& num);
+
+string remove_odd_node(string src);
